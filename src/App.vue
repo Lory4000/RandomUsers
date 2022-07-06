cb3fd0c8ab89a2c74d51dffded67a0639205171a
LongLifeROC <72567303+LongLifeROC@users.noreply.github.com>
2022-07-06 19:13:34 +0200

    Resolved image issue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         lastName: "Hajdari",
         email: "lory.h4000@gmail.com",
         gender: "male",
-        picture: "src/assets/lory.jpg",
+        picture: "lory.png",
         location: {
           street: {
             number: 15,
